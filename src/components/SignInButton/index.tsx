d169a492e33d05be85dd136cd71cb2e2e70a3db0
tharlys10 <tharlys2015c@gmail.com>
2021-11-01 12:15:31 -0300

    feat: :sparkles: implementation next auth with GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,27 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [ session ] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       className={styles.singInButton}
       type="button"
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Tharlys Alves
+      { session.user.name }
       <FiX className={styles.closeIcon} color="#737380" />
     </button>
   ) : (
     <button
       className={styles.singInButton}
       type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sing in with GitHub
