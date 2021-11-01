ddb44db4376ddc14085cbe00bed97982ceeb92c5
tharlys10 <tharlys2015c@gmail.com>
2021-11-01 18:15:12 -0300

    feat: :sparkles: subscribe user in stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,15 +1,45 @@
 
+import { useSession, signIn } from 'next-auth/client';
+
+import { api } from '../../services/api';
+
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [ session ] = useSession();
+  
+  async function handleSubscribe(){
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      
+      console.log(response);
+      
+      const { session_id } = response.data;
+
+      const stripe = await getStripeJs();
+      
+      await stripe.redirectToCheckout({ sessionId: session_id })
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+  
   return (
     <button
       className={styles.subscribeButton}
       type="button"
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
